6dd9038151e278c632d738267ca9f8039c2bdc39
zahra babaei <zahrababaei830@gmail.com>
2025-03-02 12:31:35 +0330

    E5.2

--- Main.java
+++ Main.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter your number:");
+        double num = in.nextDouble();
+        if (num == 0) {
+            System.out.println("zero");
+        } else if (num > 0) {
+            System.out.println("positive");
+        } else {
+            System.out.println("negative");
+        }
+        //large or small
+        if (Math.abs(num) > 1000000) {
+            System.out.println("large");
+        } else if (Math.abs(num) < 1) {
+            System.out.println("small");
+        }
     }
 }
