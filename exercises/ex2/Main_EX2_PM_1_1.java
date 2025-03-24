119a3a1516c2d44b6ba3edae60e64e9f0ac39687
zahra babaei <zahrababaei830@gmail.com>
2025-03-24 14:15:37 +0330

    EX2_PM_1_1

---
+++ exercises/ex2/Main_EX2_PM_1_1.java
@@ -0,0 +1,26 @@
+package ap.exercises.ex2;
+import java.util.Scanner;
+
+public class Main_EX2_PM_1_1 {
+    public static void main(String[] args) {
+        System.out.println("enter n:");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.close();
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.print("*\t");
+        }
+        System.out.print("\n");
+        for (int i = 1; i < n - 1; i++) {
+            System.out.print("*\t");
+            for (int j = 1; j < n - 1; j++) {
+                System.out.print("\t");
+            }
+            System.out.println("*");
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print("*\t");
+        }
+    }
+}
