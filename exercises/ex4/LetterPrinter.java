5f660cfab94bc80565c2814f73759653b0100522
zahra babaei <zahrababaei830@gmail.com>
2025-04-29 22:07:09 +0330

    EX4_E3_15

--- exercises/ex4/LetterPrinter.java
+++ exercises/ex4/LetterPrinter.java
@@ -23,7 +23,6 @@ class Letter {
 
 public class LetterPrinter {
     public static void main(String[] args) {
-
         Letter letter = new Letter("Mary", "John");
         letter.addLine("I am sorry we must part.");
         letter.addLine("I wish you all the best.");
