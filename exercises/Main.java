e27fd55b0cfa05a51440d95207b6528a0bb96020
zahra babaei <zahrababaei830@gmail.com>
2025-05-06 13:25:14 +0330

    quiz_E2

--- exercises/Main.java
+++ exercises/Main.java
@@ -1,39 +1,54 @@
 package ap.exercises;
+
 import java.util.ArrayList;
 
-class Pen{
+class Pen {
     private String color;
-    private int price;
+    private double price;
     private String brand;
 
-    public Pen(String color, int price, String brand){
+    public Pen(String color, double price, String brand) {
         this.color = color;
         this.price = price;
         this.brand = brand;
     }
-    public String getColor(){
+
+    public String getColor() {
         return color;
     }
-    public int getPrice(){
+
+    public double getPrice() {
         return price;
     }
-    public String getBrand(){
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getBrand() {
         return brand;
     }
 }
-class Book{
-    private String title;
-    private int price;
 
-    public Book(String title, int price){
+class Book {
+    private String title;
+    private double price;
+
+    public Book(String title, double price) {
         this.title = title;
         this.price = price;
     }
-    public String getTitle(){
+
+    public String getTitle() {
         return title;
     }
-    public int getPrice(){
+
+    public double getPrice() {
         return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 }
 
@@ -41,17 +56,25 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Pen> pen = new ArrayList<>();
         ArrayList<Book> book = new ArrayList<>();
-        for(int i = 0; i < 2; i++){
-            pen.add(new Pen("color"+(i+1),100+i,"brand"+(i+1)));
-            book.add(new Book("title"+(i+1),100+i));
+        for (int i = 0; i < 2; i++) {
+            pen.add(new Pen("color" + (i + 1), 100 + i, "brand" + (i + 1)));
+            book.add(new Book("title" + (i + 1), 100 + i));
         }
-        for(Book b : book){
-            System.out.println(b.getTitle()+" "+b.getPrice());
+        for (Book b : book) {
+            System.out.println(b.getTitle() + "\nbefore discount:" + b.getPrice() + "\tafter discount:" + discount(b.getPrice()));
+            System.out.println("-------");
         }
-        System.out.println("-------------");
-        for(Pen p : pen){
-            System.out.println(p.getColor()+" "+p.getPrice()+" "+p.getBrand());
+        System.out.println("---------------------------");
+        for (Pen p : pen) {
+            System.out.println(p.getColor() + " " + p.getBrand() + "\nbefore discount:" + p.getPrice() + "\tafter discount:" + discount(p.getPrice()));
+            System.out.println("-------");
         }
+
+    }
+
+    public static double discount(double price) {
+        price -= (price * 0.1);
+        return price;
 
     }
 }
