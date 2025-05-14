5762b0f44255e812d85f8f541df59055fb10ae4c
zahra babaei <zahrababaei830@gmail.com>
2025-05-14 23:23:10 +0330

    mid_project_part1

---
+++ mid_project/Main.java
@@ -0,0 +1,9 @@
+package ap.mid_project;
+
+public class Main {
+    public static void main(String[] args) {
+        Library library = new Library(false,false);
+        Menu menu = new Menu();
+        menu.showMenu();
+    }
+}
